330cd56b40019104967f8e3c9ac524dcf5c12f38
tcasey <hello@tcasey.me>
2017-04-08 00:34:06 -0600

    navbar :nail-care:

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,10 +19,10 @@ class Home extends Component {
             </div>
             <div className='home-content'>
               <p>
-                I'm a creative professional and front-end developer with an obession for clean code and an eye for design
+                I'm a creative developer with an obession for clean code and an eye for design
               </p>
               <p>
-                I studied Information Systems at the University of Utah and attended an immersive bootcamp at DevMountain focused on full stack JavaScript Development. Currently I'm working as a JavaScript Developer at
+                I'm currently working as a Full Stack JavaScript Developer at
                 <a className='employer' target='blank' href='https://www.vivintsolar.com'> Vivint.solar</a>.
               </p>
               <p className='paragraph-link'>Check out my

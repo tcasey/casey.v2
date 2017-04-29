a7229bfbf142915ace2a0a8cfd60d8d9af2e2fde
tcasey <hello@tcasey.me>
2017-04-29 12:23:50 -0600

    :fire: base

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -38,7 +38,7 @@ class Home extends Component {
                   target='blank'
                   href='https://www.vivintsolar.com'
                 >
-                Vivint.solar
+                 Vivint.solar
                 </a>.
               </p>
               <p className='paragraph-link'>Check out my
